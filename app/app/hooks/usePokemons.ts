2d4c746696aaa232965ff6f23a46f78d15af2365
Luis Alaguna <alagunasalahaddin@live.com>
2025-02-22 02:11:15 -0500

    chore: handle fetching when the application is focused

--- app/app/hooks/usePokemons.ts
+++ app/app/hooks/usePokemons.ts
@@ -1,11 +1,15 @@
 import { fetchPokemons } from "../api/service";
 import { useQuery } from "@tanstack/react-query";
 import { PokemonDetail } from "../domain/pokemon.model";
+import { useIsFocused } from "@react-navigation/native";
 
 const usePokemons = () => {
+  const isFocused = useIsFocused();
+
   const { data, error, isLoading } = useQuery<Array<PokemonDetail>, Error>({
     queryKey: ["pokemons"],
     queryFn: fetchPokemons,
+    subscribed: isFocused,
   });
 
   return { data, error, isLoading };
