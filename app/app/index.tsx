816dd8a8e093f6d97a3d364b4953e9a0d3f99acd
Luis Alaguna <alagunasalahaddin@live.com>
2025-02-22 13:24:06 -0500

    feat: add scheleton for firabase login

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,5 +1,9 @@
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { StyleSheet } from "react-native";
+
+// import "./firebase";
 import PokeList from "./screens/PokeList";
 
 const queryClient = new QueryClient();
@@ -7,9 +11,36 @@ const queryClient = new QueryClient();
 export default function HomeScreen() {
   useReactQueryDevTools(queryClient);
 
+  /* 
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
+  const [initializing, setInitializing] = useState(true);
+
+  const onAuthStateChanged = (user: FirebaseAuthTypes.User | null) => {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  };
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+
+  if (initializing)
+    return (
+      <LoaderScreen message={"Loading pokemons..."} color={Colors.grey40} />
+    );
+
+  if (!user) {
+    return <LoginScreen />;
+  }
+ */
   return (
     <QueryClientProvider client={queryClient}>
       <PokeList />
     </QueryClientProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  loader: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
---
+++ app/app/screens/LoginScreen.tsx
@@ -0,0 +1,51 @@
+import auth from "@react-native-firebase/auth";
+import { useState } from "react";
+import { StyleSheet } from "react-native";
+import { Button, Colors, Text, TextField, View } from "react-native-ui-lib";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleLogin = () => {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => setError(error.message));
+  };
+
+  return (
+    <View gap-20 padding-20 center>
+      <Text text40BO center marginB-20>
+        Login
+      </Text>
+      <TextField
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        style={styles.input}
+      />
+      <TextField
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Button label="Login" onPress={handleLogin} style={styles.button} />
+
+      {error && (
+        <Text color={Colors.red30} marginB-10>
+          {error}
+        </Text>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: { marginBottom: 15 },
+  button: { marginTop: 10 },
+});
+
+export default LoginScreen;
